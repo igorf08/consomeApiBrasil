02d818190216ada823c3b40eface9554230421a8
igorf08 <igorfmaa@gmail.com>
2026-08-01 14:35:17 -0300

    fix: Corrige nome da aplicação, tornando-a homogenea

--- src/main/java/com/igorf08/consomeApiBrasil/controller/IndexController.java
+++ src/main/java/com/igorf08/consomeApiBrasil/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.igorf08.requisicaoApi.controller;
+package com.igorf08.consomeApiBrasil.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
